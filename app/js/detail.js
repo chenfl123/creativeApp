7543b45a6b35f77693f1ad3190d96416df0dfcc7
cfling <1606039807@qq.com>
2016-11-01 19:50:40 +0800

    详情页加入购物车

--- app/js/detail.js
+++ app/js/detail.js
@@ -1,3 +1,14 @@
 Zepto(function($){
-  
+  var $addcar= $('.addcar');
+  var $number =$('.number');
+  var $pay =$('.pay');
+  var shopString= localStorage.getItem('shopString');
+  shopString=shopString ?JSON.parse(shopString):[];
+	$addcar.on('singleTAB',function(){
+		var goods={};
+		// goods.id=$('.d_show').find('img').nodeName;
+		console.log($('.d_show').find('img').nodeName);
+
+	})
+
 })
