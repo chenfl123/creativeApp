110d91da08acef2be31cb8b68da82ec4acd04a95
cfling <1606039807@qq.com>
2016-11-03 18:08:02 +0800

    解决详情页加入购物车的bug

--- app/js/detail.js
+++ app/js/detail.js
@@ -1,75 +1,91 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // 商品轮播图
+    var mySwiper2 = new Swiper('.swiper-container', {
+        // 自动播放
+        autoplay: 5000,
+        loop: true,
 
-
-document.addEventListener('DOMContentLoaded',function(){
-  // 商品轮播图
-var mySwiper2 = new Swiper('.swiper-container', {
-            // 自动播放
-            autoplay: 5000,
-            loop: true,
-          
-        });
-	
-	//切换
-    $(".container-fluid .title span").first().on("click",function(){
-    	
-    	$(this).siblings().removeClass("active");
-    	$(this).first().addClass("active");
-    	
     });
-     $(".container-fluid .title span").last().on("click",function(){
-    
-    	$(this).siblings().removeClass("active");
-    	$(this).last().addClass("active");
-    	
+
+    //切换
+    $(".container-fluid .title span").first().on("singleTap", function() {
+
+        $(this).siblings().removeClass("active");
+        $(this).first().addClass("active");
+
     });
-    
+    $(".container-fluid .title span").last().on("singleTap", function() {
 
-  var $addcar= $('.addcar');
-  var $number =$('.number');
-  var $pay =$('.pay'); 
+        $(this).siblings().removeClass("active");
+        $(this).last().addClass("active");
 
-  var shopString= localStorage.getItem('shopString');
-  shopString=shopString ?JSON.parse(shopString):[];
+    });
 
-//arr数组收集所有商品id
-  var arr=[];
-// carnum购物车数量
 
-  var carnum=0;
-  if (shopString!=[]) {
-  	 $.each(shopString,function(idx, item){
-  		arr.push(item.id);
-  		carnum +=parseInt(item.number);
-  	})
-  }
+    var $addcar = $('.addcar');
+    var $number = $('.number');
+    var $pay = $('.pay');
 
-  	$number.html(carnum);
 
-  	// 加入购物车设置数据
 
-	$addcar.on('singleTap',function(){
-		var goods={};
-		var Id =$('.d_show img').attr('id');
-		var numb=1;
+    //arr数组收集所有商品id
+    var arr = [];
+    var carnum = 0;
+    // carnum购物车数量
+    var shopString = localStorage.getItem('shopString');
+    shopString = shopString ? JSON.parse(shopString) : [];
+    if (shopString != []) {
+        $.each(shopString, function(idx, item) {
+            arr.push(item.id);
+            carnum += parseInt(item.number);
+        })
+    }
+rest();
+    $number.html(carnum);
 
-		for(var i=0;i<arr.length;i++){
-			numb=shopString[i].number;
-		if (arr[i]==Id) {
-			shopString.splice(i,1);
-			numb=numb+1;
-			break;
+   
+    // 加入购物车设置数据
 
-			}else{numb=1;}
-		}
-		goods.id=Id;
-		goods.number=numb;
-		goods.title=$('.d_show img').attr('alt');
-		goods.price=$('.detail .price span').html();
-		goods.imgurl=$('.d_show img').attr('src');
-	
-		shopString.push(goods); 
-		localStorage.setItem('shopString',JSON.stringify(shopString));
-	})
+    $addcar.on('singleTap', function() {
+      rest();
+        var goods = {};
+        var Id = $('.d_show img').attr('id');
+        var numb = 1;
 
-	
-})	
+        for (var i = 0; i < arr.length; i++) {
+
+            if (arr[i] == Id) {
+                numb = shopString[i].number + 1;
+                shopString.splice(i, 1);
+
+                break;
+            }
+        }
+
+        goods.id = Id;
+        goods.number = numb;
+        goods.title = $('.d_show img').attr('alt');
+        goods.price = $('.detail .price span').html();
+        goods.imgurl = $('.d_show img').attr('src');
+
+        shopString.push(goods);
+        localStorage.setItem('shopString', JSON.stringify(shopString));
+        // 实时更新购物车数目
+        $number.html(++carnum);
+    })
+
+ function rest() {
+      arr.length=0;
+      carnum=0;
+        var shopString = localStorage.getItem('shopString');
+        shopString = shopString ? JSON.parse(shopString) : [];
+        if (shopString != []) {
+            $.each(shopString, function(idx, item) {
+                arr.push(item.id);
+                carnum += parseInt(item.number);
+            })
+        }
+
+    }
+
+})
